0c94e36a578c838f9173ac366fc73173ab926894
sedajorgensencareer <sedajorgensen.career@gmail.com>
2025-12-13 11:28:04 +1300

    Add s01_config.py file

---
+++ s01_config.py
@@ -0,0 +1,26 @@
+import sys
+assert sys.version_info >= (3, 7)
+
+from packaging import version
+import sklearn
+
+assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
+
+import matplotlib.pyplot as plt
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+from pathlib import Path
+
+IMAGES_PATH = Path() / "images" / "classification"
+IMAGES_PATH.mkdir(parents=True, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+            path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+            if tight_layout:
+                plt.tight_layout()
+            plt.savefig(path, format=fig_extension, dpi=resolution)
