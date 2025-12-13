17a1fdab7555d33654c1a0cf07598a7fff138558
sedajorgensencareer <sedajorgensen.career@gmail.com>
2025-12-13 19:23:42 +1300

    load_mnist.py update

--- s02_load_mnist.py
+++ s02_load_mnist.py
@@ -23,25 +23,25 @@ def create_mnist_test_train():
 X_train, X_test, y_train, y_test = create_mnist_test_train()
 
 ## Exploration of input features (how they're presented)
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-def plot_digit(image_data):
-    image = image_data.reshape(28, 28)
-    plt.imshow(image, cmap="binary")
-    plt.axis("off")
+# def plot_digit(image_data):
+#     image = image_data.reshape(28, 28)
+#     plt.imshow(image, cmap="binary")
+#     plt.axis("off")
     
-some_digit = X_train[0]
-plot_digit(some_digit)
-save_fig("some_digit_plot")
-plt.show()
+# some_digit = X_train[0]
+# plot_digit(some_digit)
+# save_fig("some_digit_plot")
+# plt.show()
 
 # print(y_train[0])
 
 ## Create a 10x10 image containing the first 100 digits in the training set
-plt.figure(figsize=(9,9))
-for idx, image_data in enumerate(X_train[:100]):
-    plt.subplot(10, 10, idx + 1)
-    plot_digit(image_data)
-plt.subplots_adjust(wspace=0, hspace=0)
-save_fig("100_digits_visualised", tight_layout=False)
-plt.show()
+# plt.figure(figsize=(9,9))
+# for idx, image_data in enumerate(X_train[:100]):
+#     plt.subplot(10, 10, idx + 1)
+#     plot_digit(image_data)
+# plt.subplots_adjust(wspace=0, hspace=0)
+# save_fig("100_digits_visualised", tight_layout=False)
+# plt.show()
